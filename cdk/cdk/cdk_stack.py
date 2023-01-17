0858cacacac1c39947abca812222496eed365ee0
Andrew Violette <andrew@andrewviolette.net>
2023-01-17 14:20:11 -0600

    working cdk stack

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,7 +1,13 @@
 from aws_cdk import (
     Stack,
 )
-from aws_cdk import aws_ec2 as ec2, aws_ecs as ecs, aws_ecs_patterns as ecs_patterns
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+    aws_dynamodb as dynamodb,
+)
 import aws_cdk.aws_ecr as ecr
 
 from constructs import Construct
@@ -15,7 +21,7 @@ class CdkStack(Stack):
 
         cluster = ecs.Cluster(self, "FskCluster", vpc=vpc)
 
-        ecs_patterns.ApplicationLoadBalancedFargateService(
+        fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
             self,
             "FreudianSearch",
             cluster=cluster,
@@ -27,8 +33,43 @@ class CdkStack(Stack):
                     ecr.Repository.from_repository_name(
                         self, "FskRepository", "fskboot"
                     )
-                )
+                ),
             ),
             memory_limit_mib=2048,
             public_load_balancer=True,
         )
+
+        table = dynamodb.Table(
+            self,
+            id="FreudianSearchTable",
+            table_name="FreudianSearch",
+            partition_key=dynamodb.Attribute(
+                name="pk", type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(name="sk", type=dynamodb.AttributeType.STRING),
+        )
+
+        table.add_global_secondary_index(
+            index_name="GSI1",
+            partition_key=dynamodb.Attribute(
+                name="gsi1pk", type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name="gsi1sk", type=dynamodb.AttributeType.STRING
+            ),
+        )
+        dynamo_db_endpoint = vpc.add_gateway_endpoint(
+            "DynamoDbEndpoint", service=ec2.GatewayVpcEndpointAwsService.DYNAMODB
+        )
+        dynamo_db_endpoint.add_to_policy(
+            iam.PolicyStatement(
+                principals=[iam.AnyPrincipal()],
+                actions=[
+                    "dynamodb:DescribeTable",
+                    "dynamodb:PutItem",
+                    "dynamodb:ListTables",
+                ],
+                resources=["*"],
+            )
+        )
+        table.grant_full_access(fargate_service.task_definition.task_role)
